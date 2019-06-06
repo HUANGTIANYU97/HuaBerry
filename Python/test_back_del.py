777f4487008896d4c1c61bc3ad34f580793c1abb
TianyangCHEN <tianyang.chen@etu.univ-cotedazur.fr>
2019-06-06 20:37:07 +0200

    len delete

--- Python/test_back_del.py
+++ Python/test_back_del.py
@@ -1,6 +1,6 @@
 import paho.mqtt.publish as publish
 
 def delete():
-  publish.single("button", payload="buttonDelte", qos=0, hostname="172.20.10.8")
+  publish.single("button", payload="buttonDelete", qos=0, hostname="172.20.10.8")
 
 delete()
